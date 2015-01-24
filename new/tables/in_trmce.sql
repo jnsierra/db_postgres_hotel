5c0105124f6d30f44031e0bfefe1fe6fcf508cf2
jnsierra <jnsierrac@gmail.com>
2015-01-23 23:49:28 -0500

    23/01/2015
    Tabla de remisiones de celualres

---
+++ new/tables/in_trmce.sql
@@ -0,0 +1,23 @@
+-- CREAMOS LA TABLA REMISIONES para CELULARES
+-- Tabla en la cual se almacenaran los datos de los celulares que entran en consignacion
+-- 
+
+--DROP TABLE IF EXISTS em_tpara;
+
+CREATE TABLE in_trmce(
+   rmce_rmce                SERIAL                       ,
+   rmce_rfce                int                          ,
+   rmce_imei                VARCHAR(150)                 ,
+   rmce_iccid               VARCHAR(150)                 ,
+   rmce_valor               NUMERIC(50,6)                ,
+   rmce_comision            NUMERIC(50,6)                ,
+   rmce_tppl                VARCHAR(2)                   ,
+   rmce_fcve                date                         ,
+   rmce_fcsl                date                         ,
+   rmce_fcen                date default NOW()           ,
+   rmce_tius_ent            int                          ,
+   rmce_tius_sal            int                          ,
+   rmce_codigo              VARCHAR(2)                   ,
+   rmce_sede                int                          ,
+   PRIMARY KEY (rmce_rmce)
+);
