97151a881ef6d718e2b2bf9e0ea02f599e4be23f
jnsierra <jnsierrac@gmail.com>
2015-02-15 12:14:31 -0500

    15/02/2015
    Correccion de datos de cliente

--- new/functions/US_FINSERT_CLIENTE.sql
+++ new/functions/US_FINSERT_CLIENTE.sql
@@ -3,59 +3,47 @@
 CREATE OR REPLACE FUNCTION US_FINSERT_CLIENTE (    
                                             p_nombres           varchar(50), 
                                             p_apellidos         varchar(50), 
-                                            p_cedula            varchar(50), 
+                                            p_cedula            numeric, 
                                             p_email             varchar(50), 
-                                            p_fecha_nac         varchar(50), 
-                                            p_telefono          varchar(50), 
-                                            p_celular           varchar(50), 
-                                            p_direccion         varchar(50),
-                                            p_departamentoRes   varchar(50), 
-                                            p_ciudadResi        varchar(50)
+                                            p_telefono          varchar(50)                                            
                                             ) RETURNS varchar AS $$
     DECLARE
                                             
-    c_pers_cliente cursor for
-    select coalesce(pers_pers,-1)
-    from us_tpers
-    where pers_cedula = trim(p_cedula)
-    and pers_email = trim(p_email)
-    ;
+    c_cliente cursor for
+    SELECT count(*)
+      FROM us_tclien
+     WHERE clien_cedula = p_cedula
+    ;        
     
-    c_clien_clien cursor for
-    select coalesce(clien_clien,-1)
-    from us_tclien, us_tpers
-    where pers_cedula = trim(p_cedula)
-    and pers_email = trim(p_email)
-    and pers_pers = clien_pers
-    ;
+    v_clien_cedula    numeric(10,0);
     
-    
-    v_pers_pers      int;
-    v_clien_clien    int;
-                                            
     BEGIN
     
-    insert into us_tpers (pers_apellido, pers_nombre, pers_cedula, pers_email, pers_fecha_nac, pers_tel, pers_cel, pers_dir, pers_dept_resi, pers_ciudad_resi)
-    values (upper(trim(p_apellidos)), upper(trim(p_nombres)),trim(p_cedula), trim(p_email),to_date(p_fecha_nac,'dd/mm/yy'), trim(p_telefono),trim(p_celular), trim(p_direccion), trim(p_departamentoRes), trim(p_ciudadResi) );   
+    OPEN c_cliente;
+    FETCH c_cliente INTO v_clien_cedula;
+    CLOSE c_cliente;
     
-    OPEN c_pers_cliente;
-    FETCH c_pers_cliente INTO v_pers_pers;
-    CLOSE c_pers_cliente;
-    
-    IF v_pers_pers <> -1 THEN
-        
-        insert into us_tclien (clien_pers)
-        values(v_pers_pers);
-        
-        OPEN c_clien_clien;
-        FETCH c_clien_clien INTO v_clien_clien;
-        CLOSE c_clien_clien;
-        
+    IF v_clien_cedula = 0 THEN
+        --
+        INSERT INTO us_tclien(
+            clien_cedula, 
+            clien_nombres, 
+            clien_apellidos, 
+            clien_telefono, 
+            clien_correo)
+        VALUES (
+                p_cedula, 
+                p_nombres, 
+                p_apellidos, 
+                p_telefono, 
+                p_telefono);
+        RETURN 'Ok';
+        --        
+    ELSE
+        --
+        RAISE EXCEPTION ' El usuario con cedula % que desea ingresar ya existe en la base de datos ',p_cedula ;
+        --
     END IF;
-    
-    
-    
-    RETURN 'OK-' || v_clien_clien;
     
     EXCEPTION WHEN OTHERS THEN
          RETURN 'Err' || sqlerrm;
