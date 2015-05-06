15ecd386c9d98875de9c97e2e489b8f8f43cb996
jnsierra <jnsierrac@gmail.com>
2015-05-05 21:33:59 -0500

    05/05/2015 01
    Solucion del chicharron al facturar

--- new/functions/US_FINSERT_CLIENTE.sql
+++ new/functions/US_FINSERT_CLIENTE.sql
@@ -14,30 +14,44 @@ CREATE OR REPLACE FUNCTION US_FINSERT_CLIENTE (
       FROM us_tclien
      WHERE clien_cedula = p_cedula
     ;        
-    
+    --
     v_clien_cedula    numeric(10,0);
-    
+    --
+    v_clien_clien     int;
+    --
+    c_clien_clien CURSOR FOR
+    SELECT nextval('us_tclien_clien_clien_seq')
+    ;
+    --
     BEGIN
-    
+    --
     OPEN c_cliente;
     FETCH c_cliente INTO v_clien_cedula;
     CLOSE c_cliente;
+    --
     
     IF v_clien_cedula = 0 THEN
         --
+        OPEN c_clien_clien;
+        FETCH c_clien_clien INTO v_clien_clien;
+        CLOSE c_clien_clien;
+        --
         INSERT INTO us_tclien(
+            clien_clien,
             clien_cedula, 
             clien_nombres, 
             clien_apellidos, 
             clien_telefono, 
             clien_correo)
         VALUES (
+                v_clien_clien,
                 p_cedula, 
                 p_nombres, 
                 p_apellidos, 
                 p_telefono, 
                 p_email);
-        RETURN 'Ok';
+        --
+        RETURN 'Ok-'||v_clien_clien;
         --        
     ELSE
         --
