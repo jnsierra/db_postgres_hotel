05fe138eb640767c34dbbc5dd98bfd858e190fce
jnsierra <jnsierrac@gmail.com>
2015-08-12 23:00:32 -0500

    12/08/2015
    Eliminacion de restricciones inecesarias

--- new/checks/in_tcate_chks.sql
+++ new/checks/in_tcate_chks.sql
@@ -1,11 +1,3 @@
 ALTER TABLE in_tcate 
 ADD CONSTRAINT cate_estado_chk 
 CHECK (cate_estado in ('A','I'));
-
-ALTER TABLE in_tcate 
-ADD CONSTRAINT cate_runic_chk 
-CHECK (cate_runic in ('S','N'));
-
-ALTER TABLE in_tcate 
-ADD CONSTRAINT cate_feven_chk 
-CHECK (cate_feven in ('S','N'));
