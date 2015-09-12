248505430eac21589c532b6f2d0329a74ea2cda7
root <johnmorenoing@gmail.com>
2015-09-12 17:15:56 -0500

    Tabla de domicilios

---
+++ new/comments/tdomi_com.sql
@@ -0,0 +1,6 @@
+COMMENT ON TABLE  fa_tdomi                          IS          'Tabla encargada de llevar control de las facturas que son realizadas por domicilio';
+COMMENT ON COLUMN fa_tdomi.domi_domi                IS          'Identificador primario de la tabla';
+COMMENT ON COLUMN fa_tdomi.domi_fact                IS          'Identifica el id de la factura que se realizo';
+COMMENT ON COLUMN fa_tdomi.domi_clien               IS          'Identifica el cliente de la factura.';
+COMMENT ON COLUMN fa_tdomi.domi_estado              IS          'Estado del domicilio';
+
