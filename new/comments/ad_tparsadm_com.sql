98043a66bcb71342ccbf73a7eb15e15c96d40d80
root <johnmorenoing@gmail.com>
2015-10-13 06:46:31 -0500

    Campos parametrizables

---
+++ new/comments/ad_tparsadm_com.sql
@@ -0,0 +1,5 @@
+COMMENT ON TABLE   AD_TPARSADM IS 'Tabla encargada de almacenar parametros adicionales, dependiendo de cada empresa';
+COMMENT ON COLUMN  AD_TPARSADM.PARSADM_PARSADM IS 'Identificador primario de la tabla';
+COMMENT ON COLUMN  AD_TPARSADM.PARSADM_NOMBRE  IS 'Nombre del parametro a ingresar';
+COMMENT ON COLUMN  AD_TPARSADM.PARSADM_ESTADO  IS 'Estado del parametro a ingresar';
+COMMENT ON COLUMN  AD_TPARSADM.PARSADM_VALOR   IS 'Valor del parametro a ingresar';
